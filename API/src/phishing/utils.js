e7596b16508ede846ad2059411c69d85ff917140
AlexEW <alexelliswilson@gmail.com>
2024-05-07 10:37:41 +0100

    added refined models

--- API/src/phishing/utils.js
+++ API/src/phishing/utils.js
@@ -1,6 +1,6 @@
 //machine learning model locations
-const text_model_url = "API/src/models/sklearn_text_model.onnx";
-const url_model_url = "API/src/models/sklearn_url_model.onnx";
+const text_model_url = "API/src/models/sklearn_text_model_Strip_Unicode.onnx"; //optimised model
+const url_model_url = "API/src/models/sklearn_url_model_Strip_unicode_ng2.onnx"; //optimised model
 
 //export util functions
 module.exports = { 
