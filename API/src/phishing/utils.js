9b5f49b3970ffa74f48c7463500f62030632ee53
AlexEW <alexelliswilson@gmail.com>
2024-05-07 11:42:49 +0100

    Adding CNB model for testing on heroku

--- API/src/phishing/utils.js
+++ API/src/phishing/utils.js
@@ -2,7 +2,7 @@
 //optimised model
 const text_model_url = "API/src/models/sklearn_text_model_Strip_Unicode.onnx";
 //base model, heroku cannot run the optimised model for memory issues
-const url_model_url = "API/src/models/sklearn_url_model.onnx"; 
+const url_model_url = "API/src/models/sklearn_url_model_CNB.onnx"; 
 
 //export util functions
 module.exports = { 
